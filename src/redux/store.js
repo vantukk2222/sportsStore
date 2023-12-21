068928a5fd5eb323b5f60276689fc1697a97a15f
Hieu220302 <hieupiro124@gmail.com>
2023-12-21 18:53:14 +0700

    cm

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+const rootReducer = combineReducers({
+
+});
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+});
+export const persistor = persistStore(store);
