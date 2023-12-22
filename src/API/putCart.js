80a9e1ccada7f9d5d3ddd085313c937de6fbf4e9
Hieu220302 <hieupiro124@gmail.com>
2023-12-22 16:25:18 +0700

    cm

--- src/API/putCart.js
+++ src/API/putCart.js
@@ -3,11 +3,11 @@ import { api } from './url';
 const putCart = async (id, quantity, authToken) => {
     try {
         console.log(authToken);
-        await axios.put(`${api}cart/change-quantity/${id}?quantity=${quantity}`, {
+        await axios.put('https://project-pbl6-production.up.railway.app/api/v1/cart/change-quantity/10?quantity=5', {
             headers: {
                 'Content-Type': 'application/json',
                 accept: '*/*',
-                Authorization: `Bearer ${authToken}`,
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0MTAiLCJyb2xlIjpbIlJPTEVfQ1VTVE9NRVIiXSwiaWF0IjoxNzAzMjM1MDU1LCJleHAiOjE3MDMyNTMwNTV9.j32DGDe3_FadfiIQf48CWsCMgL4hgQFd8G4rrhY-sjk`,
             },
         });
     } catch (error) {
