c7c9439f76f69cc0c110c160add1909c7a3a8a05
baopro1cute123 <nguyenbao12c6nh1@gmail.com>
2024-01-04 01:40:00 +0700

    123

--- src/components/pageAdmin/Admin.jsx
+++ src/components/pageAdmin/Admin.jsx
@@ -1,15 +1,13 @@
 import './Admin.css';
-import Dashboard from './Dashboard/Dashboard';
-import MenuAdmin from './MenuAdmin';
+import AdminProduct from './Product/AdminProduct';
 
 const Admin = () => {
     return (
         <section className="shop">
             <div className="d_flex">
-                <MenuAdmin />
                 <div className="contentWidth">
                     <div className="heading d_flex"></div>
-                    <Dashboard />
+                    <AdminProduct />
                 </div>
             </div>
         </section>
